f94ce362715a41da2d8921baeada87e295af2ce2
raouf hamaissia <r.hamaissia@gmail.com>
2026-07-07 01:08:34 +0100

    Workspace registration form created

---
+++ accounts/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from accounts.models import Workspace, User
+
+class WorkspaceForm(forms.Form):
+    workspace_name = forms.CharField(
+        max_length=255,
+        widget=forms.TextInput(attrs={'placeholder': 'Company Name'})
+        
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'placeholder': 'admin@company.com'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Secure Password'})
+    )
+    
+    def clean_workspace_name(self):
+        name = self.cleaned_data.get('workspace_name')
+        if Workspace.objects.filter(name__iexact=name).exists():
+            raise forms.ValidationError("A workspace with this name already exists.")
+        return name
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email__iexact=email).exists():
+            raise forms.ValidationError("A user with this email already exists.")
+        return email
